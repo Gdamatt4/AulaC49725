fefa0b25a0391b3277d26497c1b03815519ebf4d
Gabriel da Mata <whey@mizeravi.com>
2015-10-30 16:07:51 -0300

    Hello world em C

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h<>
+#include <stdlib.h>
+
+int main(int argo, char *argv[])
+(
+ printf("HELLO WORLD");
+ [][][][][][][][][][][][][][][][][][][][]
+ system ("PAUSE");
+ return 0;
+ )
